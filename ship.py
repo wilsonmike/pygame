eef402af8f4ee048c1615fca214b51e14952748d
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-14 12:20:23 -0400

    hold move left working

--- ship.py
+++ ship.py
@@ -11,10 +11,13 @@ class Ship:
         
         self.rect.midbottom = self.screen_rect.midbottom
         self.moving_right = False
+        self.moving_left = False
     
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.rect.x += .5
+        if self.moving_left:
+            self.rect.x += .5
     
     def blitme(self):
         self.screen.blit(self.image, self.rect)
