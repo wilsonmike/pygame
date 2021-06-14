a358afec8365d1c8e4b46648ab22e05e8c970f9a
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-14 12:39:16 -0400

    left and right hold working - dynamic speed

--- ship.py
+++ ship.py
@@ -3,21 +3,22 @@ import pygame
 class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-        
         self.image = pygame.image.load('images/spaceship.bmp')
         self.image = pygame.transform.scale(self.image, (60, 60))
         self.rect = self.image.get_rect()
-        
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         if self.moving_right:
-            self.rect.x += .5
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x += .5
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
     
     def blitme(self):
         self.screen.blit(self.image, self.rect)
