d06ce96e1ce1e4e94b72444164f58360a26ec6f1
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-16 11:33:18 -0400

    changed ship && added alien ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,7 +19,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
+            print(len(self.bullets))
             self._update_screen()
             
 
@@ -49,8 +50,16 @@ class AlienInvasion:
             self.ship.moving_left = False
     
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+            
+    def _update_bullets(self):
+        self.bullets.update()
+        # delete unused bullets
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
                     
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship:
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-        self.image = pygame.image.load('images/spaceship.bmp')
+        self.image = pygame.image.load('images/spaceship (1).bmp')
         self.image = pygame.transform.scale(self.image, (60, 60))
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
