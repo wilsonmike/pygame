8d56329c6d1b572fb9dea0dafc2daefb2105f62a
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-16 12:24:26 -0400

    refactored create fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -59,10 +59,14 @@ class AlienInvasion:
         number_aliens_x = available_space_x // (2 * alien_width)
         # creating first row
         for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+    
+    def _create_alien(self, alien_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
     
     def _fire_bullet(self):
         if len(self.bullets) < self.settings.bullets_allowed:
