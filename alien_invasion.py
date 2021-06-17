b52d2960c11e5cdc7213c81ad043a7b0fcb4e60a
mike <michaelwilsonsj@hotmail.com>
2021-06-17 16:13:04 -0400

    added alien row, moving left, right, and down pg 306

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_aliens()
             self._update_bullets()
             print(len(self.bullets))
             self._update_screen()
@@ -66,6 +67,17 @@ class AlienInvasion:
             for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
     
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _create_alien(self, alien_number, row_number):
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
@@ -74,6 +86,10 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
     
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _fire_bullet(self):
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
