a93af8350e2e6fafdc299ecf8d25614b87b4553e
mike <michaelwilsonsj@hotmail.com>
2021-06-17 16:24:38 -0400

    added bullet collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -101,6 +101,7 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
                     
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.bullets_allowed = 3
         # alien settings
         self.alien_speed = 1
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 5
         self.fleet_direction = 1
 
         
